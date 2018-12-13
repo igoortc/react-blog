4940e13339e12472722ceac9a4f9edf5579032e5
Igor <igoortc@gmail.com>
2018-12-13 17:49:22 -0200

    Including axios and requests

---
+++ src/axios.js
@@ -0,0 +1,11 @@
+// import axios from 'axios'
+
+// const instance = axios.create({
+//     baseUrl: 'https://jsonplaceholder.typicode.com'
+// })
+
+// instance.defaults.headers.common['Authorization'] = 'AUTH_TOKEN'
+
+// export default instance
+
+// import instance from './axios'
